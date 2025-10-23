f83cd18c470fd00b7d2eb619ae5fa128290b7c17
Kolotilov Yaroslav <kolotilov_yp@magnit.ru>
2025-10-23 11:23:18 +0300

    Background Modifiers

---
+++ Assets/Packages/UnityCompose/Runtime/Api/Modifiers/Appearance/BackgroundColorModifierImpl.cs
@@ -0,0 +1,56 @@
+﻿// ReSharper disable CheckNamespace
+
+using System.Runtime.CompilerServices;
+using SharpExtensions;
+using StableCollections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UnityCompose;
+
+public static partial class ModifierExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IModifier Background(
+        this IModifier modifier,
+        Color color,
+        Optional<ComposeTransition> transition = default
+    )
+    {
+        return modifier + new BackgroundColorModifierImpl(color, transition);
+    }
+}
+
+internal class BackgroundColorModifierImpl : BaseModifier<BackgroundColorModifierImpl>
+{
+    private readonly Color _backgroundColor;
+    private readonly Optional<ComposeTransition> _transition;
+
+    public BackgroundColorModifierImpl(Color backgroundColor, Optional<ComposeTransition> transition)
+    {
+        _backgroundColor = backgroundColor;
+        _transition = transition;
+    }
+
+    public override void Apply(VisualElement element)
+    {
+        element.style.backgroundColor = _backgroundColor;
+        if (_transition.HasValue)
+            element.AddTransition(_transition.Value, "background-color");
+    }
+
+    public override void Apply(IMutableStableCollection<ComposeModifiedProperty> modifiedProperties)
+    {
+        modifiedProperties.Add(ComposeModifiedProperty.BackgroundColor);
+    }
+
+    public override void Revert(VisualElement element)
+    {
+        element.style.backgroundColor = StyleKeyword.Null;
+    }
+
+    protected override bool Equals(BackgroundColorModifierImpl other)
+    {
+        return _backgroundColor == other._backgroundColor && _transition == other._transition;
+    }
+}
---
+++ Assets/Packages/UnityCompose/Runtime/Api/Modifiers/Appearance/BackgroundImageModifierImpl.cs
@@ -0,0 +1,66 @@
+﻿// ReSharper disable CheckNamespace
+
+using System.Runtime.CompilerServices;
+using StableCollections;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityBackground = UnityEngine.UIElements.Background;
+
+namespace UnityCompose;
+
+public static partial class ModifierExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IModifier Background(this IModifier modifier, Sprite image)
+    {
+        return modifier + new BackgroundImageModifierImpl(UnityBackground.FromSprite(image));
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IModifier Background(this IModifier modifier, Texture2D image)
+    {
+        return modifier + new BackgroundImageModifierImpl(UnityBackground.FromTexture2D(image));
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IModifier Background(this IModifier modifier, VectorImage image)
+    {
+        return modifier + new BackgroundImageModifierImpl(UnityBackground.FromVectorImage(image));
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IModifier Background(this IModifier modifier, RenderTexture image)
+    {
+        return modifier + new BackgroundImageModifierImpl(UnityBackground.FromRenderTexture(image));
+    }
+}
+
+internal class BackgroundImageModifierImpl : BaseModifier<BackgroundImageModifierImpl>
+{
+    private readonly UnityBackground _background;
+
+    public BackgroundImageModifierImpl(UnityBackground background)
+    {
+        _background = background;
+    }
+
+    public override void Apply(VisualElement element)
+    {
+        element.style.backgroundImage = _background;
+    }
+
+    public override void Apply(IMutableStableCollection<ComposeModifiedProperty> modifiedProperties)
+    {
+        modifiedProperties.Add(ComposeModifiedProperty.BackgroundImage);
+    }
+
+    public override void Revert(VisualElement element)
+    {
+        element.style.backgroundImage = StyleKeyword.Null;
+    }
+
+    protected override bool Equals(BackgroundImageModifierImpl other)
+    {
+        return _background.Equals(other._background);
+    }
+}
