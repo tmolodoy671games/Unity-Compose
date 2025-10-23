3b0da6752d45b8f3c954e9492c63e8c29048cc61
Kolotilov Yaroslav <kolotilov_yp@magnit.ru>
2025-10-23 22:11:16 +0300

    Custom Transitions

---
+++ Assets/Packages/UnityCompose/Runtime/Api/Transitions/Enter/CustomEnterTransitionImpl.cs
@@ -0,0 +1,30 @@
+﻿// ReSharper disable CheckNamespace
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace UnityCompose;
+
+public static partial class ComposeFunctions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IEnterTransition Enter(Func<IBoxScope, float, LayoutInfo, IModifier> transition)
+    {
+        return new CustomEnterTransitionImpl(transition);
+    }
+}
+
+internal class CustomEnterTransitionImpl : IEnterTransition
+{
+    private readonly Func<IBoxScope, float, LayoutInfo, IModifier> _transition;
+
+    public CustomEnterTransitionImpl(Func<IBoxScope, float, LayoutInfo, IModifier> transition)
+    {
+        _transition = transition;
+    }
+
+    public IModifier Get(IBoxScope scope, float progress, LayoutInfo parent)
+    {
+        return _transition(scope, progress, parent);
+    }
+}
---
+++ Assets/Packages/UnityCompose/Runtime/Api/Transitions/Exit/CustomExitTransitionImpl.cs
@@ -0,0 +1,30 @@
+﻿// ReSharper disable CheckNamespace
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace UnityCompose;
+
+public static partial class ComposeFunctions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IExitTransition Exit(Func<IBoxScope, float, LayoutInfo, IModifier> transition)
+    {
+        return new CustomExitTransitionImpl(transition);
+    }
+}
+
+internal class CustomExitTransitionImpl : IExitTransition
+{
+    private readonly Func<IBoxScope, float, LayoutInfo, IModifier> _transition;
+
+    public CustomExitTransitionImpl(Func<IBoxScope, float, LayoutInfo, IModifier> transition)
+    {
+        _transition = transition;
+    }
+
+    public IModifier Get(IBoxScope scope, float progress, LayoutInfo parent)
+    {
+        return _transition(scope, progress, parent);
+    }
+}
