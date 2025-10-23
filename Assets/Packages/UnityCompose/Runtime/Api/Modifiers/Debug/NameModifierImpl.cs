8874edf9945e5c2eba660304c5b2c5e025f56647
Kolotilov Yaroslav <kolotilov_yp@magnit.ru>
2025-10-23 13:08:26 +0300

    Name Modifier + Replacement

---
+++ Assets/Packages/UnityCompose/Runtime/Api/Modifiers/Debug/NameModifierImpl.cs
@@ -0,0 +1,46 @@
+﻿// ReSharper disable CheckNamespace
+
+using System.Runtime.CompilerServices;
+using StableCollections;
+using UnityEngine.UIElements;
+
+namespace UnityCompose;
+
+public static partial class ModifierExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IModifier Name(this IModifier modifier, string name)
+    {
+        return modifier + new NameModifierImpl(name);
+    }
+}
+
+internal class NameModifierImpl : BaseModifier<NameModifierImpl>
+{
+    private readonly string _name;
+
+    public NameModifierImpl(string name)
+    {
+        _name = name;
+    }
+
+    public override void Apply(VisualElement element)
+    {
+        element.name = _name;
+    }
+
+    public override void Apply(IMutableStableCollection<ComposeModifiedProperty> modifiedProperties)
+    {
+        modifiedProperties.Add(ComposeModifiedProperty.Name);
+    }
+
+    public override void Revert(VisualElement element)
+    {
+        element.name = "";
+    }
+
+    protected override bool Equals(NameModifierImpl other)
+    {
+        return _name == other._name;
+    }
+}
