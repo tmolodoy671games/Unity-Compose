ef90c99effde306e35c296b592100f79d676ace2
Kolotilov Yaroslav <kolotilov_yp@magnit.ru>
2025-10-23 13:01:52 +0300

    Scale Modifier + Replacement

---
+++ Assets/Packages/UnityCompose/Runtime/Api/Modifiers/Appearance/ScaleModifierImpl.cs
@@ -0,0 +1,77 @@
+﻿// ReSharper disable CheckNamespace
+
+using System.Runtime.CompilerServices;
+using SharpExtensions;
+using StableCollections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UnityCompose;
+
+public static partial class ModifierExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IModifier Scale(
+        this IModifier modifier,
+        float scale,
+        Optional<ComposeTransition> transition = default
+    )
+    {
+        return modifier + new ScaleModifierImpl(Vector2.one * scale, transition);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IModifier Scale(
+        this IModifier modifier,
+        float scaleX,
+        float scaleY,
+        Optional<ComposeTransition> transition = default
+    )
+    {
+        return modifier + new ScaleModifierImpl(new Vector2(scaleX, scaleY), transition);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IModifier Scale(
+        this IModifier modifier,
+        Vector2 scale,
+        Optional<ComposeTransition> transition = default
+    )
+    {
+        return modifier + new ScaleModifierImpl(scale, transition);
+    }
+}
+
+internal class ScaleModifierImpl : BaseModifier<ScaleModifierImpl>
+{
+    private readonly Vector2 _scale;
+    private readonly Optional<ComposeTransition> _transition;
+
+    public ScaleModifierImpl(Vector2 scale, Optional<ComposeTransition> transition)
+    {
+        _scale = scale;
+        _transition = transition;
+    }
+
+    public override void Apply(VisualElement element)
+    {
+        element.style.scale = _scale;
+        if (_transition.HasValue)
+            element.AddTransition(_transition.Value, "scale");
+    }
+
+    public override void Apply(IMutableStableCollection<ComposeModifiedProperty> modifiedProperties)
+    {
+        modifiedProperties.Add(ComposeModifiedProperty.Scale);
+    }
+
+    public override void Revert(VisualElement element)
+    {
+        element.style.scale = StyleKeyword.Null;
+    }
+
+    protected override bool Equals(ScaleModifierImpl other)
+    {
+        return _scale.Equals(other._scale) && _transition.Equals(other._transition);
+    }
+}
