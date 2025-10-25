4500d4793ec727b0b4a5c361f9a1569aceed3667
Kolotilov Yaroslav <kolotilov_yp@magnit.ru>
2025-10-25 23:26:48 +0300

    Alignment Stretch

--- Assets/Packages/UnityCompose/Runtime/Api/Modifiers/Scopes/Alignment.cs
+++ Assets/Packages/UnityCompose/Runtime/Api/Modifiers/Scopes/Alignment.cs
@@ -12,6 +12,7 @@ public static class Alignment
     {
         Left,
         Center,
+        Stretch,
         Right,
     }
 
@@ -19,15 +20,18 @@ public static class Alignment
     {
         Top,
         Center,
+        Stretch,
         Bottom,
     }
 
     public static Horizontal Left => Horizontal.Left;
     public static Horizontal CenterHorizontally => Horizontal.Center;
+    public static Horizontal StretchHorizontally => Horizontal.Stretch;
     public static Horizontal Right => Horizontal.Right;
 
     public static Vertical Top => Vertical.Top;
     public static Vertical CenterVertically => Vertical.Center;
+    public static Vertical StretchVertically => Vertical.Stretch;
     public static Vertical Bottom => Vertical.Bottom;
 }
 
@@ -40,6 +44,7 @@ internal static class AlignmentHorizontalExtensions
         {
             Alignment.Horizontal.Left => Align.FlexStart,
             Alignment.Horizontal.Center => Align.Center,
+            Alignment.Horizontal.Stretch => Align.Stretch,
             Alignment.Horizontal.Right => Align.FlexEnd,
             _ => throw new ArgumentOutOfRangeException(nameof(alignment), alignment, null)
         };
@@ -53,6 +58,7 @@ internal static class AlignmentHorizontalExtensions
         {
             Alignment.Horizontal.Left => Justify.FlexStart,
             Alignment.Horizontal.Center => Justify.Center,
+            Alignment.Horizontal.Stretch => Justify.SpaceAround,
             Alignment.Horizontal.Right => Justify.FlexEnd,
             _ => throw new ArgumentOutOfRangeException(nameof(alignment), alignment, null)
         };
@@ -68,6 +74,7 @@ internal static class AlignmentVerticalExtensions
         {
             Alignment.Vertical.Top => Align.FlexStart,
             Alignment.Vertical.Center => Align.Center,
+            Alignment.Vertical.Stretch => Align.Stretch,
             Alignment.Vertical.Bottom => Align.FlexEnd,
             _ => throw new ArgumentOutOfRangeException(nameof(alignment), alignment, null)
         };
@@ -81,6 +88,7 @@ internal static class AlignmentVerticalExtensions
         {
             Alignment.Vertical.Top => Justify.FlexStart,
             Alignment.Vertical.Center => Justify.Center,
+            Alignment.Vertical.Stretch => Justify.SpaceAround,
             Alignment.Vertical.Bottom => Justify.FlexEnd,
             _ => throw new ArgumentOutOfRangeException(nameof(alignment), alignment, null)
         };
