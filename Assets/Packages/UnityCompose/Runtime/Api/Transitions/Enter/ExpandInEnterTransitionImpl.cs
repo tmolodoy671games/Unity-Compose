3222e19a754628aa7f909d55e493a55c4fd6f5ae
Kolotilov Yaroslav <kolotilov_yp@magnit.ru>
2025-10-26 22:18:02 +0300

    Shink /Expand

---
+++ Assets/Packages/UnityCompose/Runtime/Api/Transitions/Enter/ExpandInEnterTransitionImpl.cs
@@ -0,0 +1,57 @@
+﻿// ReSharper disable CheckNamespace
+
+using System;
+using UnityEngine;
+
+namespace UnityCompose;
+
+public static partial class ComposeFunctions
+{
+    public static IEnterTransition ExpandIn(
+        Func<Vector2>? initialScale = null,
+        IEasing? easing = null
+    )
+    {
+        return new ExpandInEnterTransitionImpl(initialScale ?? (() => Vector2.zero), easing);
+    }
+
+    public static IEnterTransition ExpandInHorizontally(
+        Func<float>? initialScaleX = null,
+        IEasing? easing = null
+    )
+    {
+        var scaleDelegate = initialScaleX ?? (() => 0f);
+        return new ExpandInEnterTransitionImpl(() => new Vector2(scaleDelegate(), 1f), easing);
+    }
+
+
+    public static IEnterTransition ExpandInVertically(
+        Func<float>? initialScaleY = null,
+        IEasing? easing = null
+    )
+    {
+        var scaleDelegate = initialScaleY ?? (() => 0f);
+        return new ExpandInEnterTransitionImpl(() => new Vector2(1f, scaleDelegate()), easing);
+    }
+}
+
+internal class ExpandInEnterTransitionImpl : IEnterTransition
+{
+    private readonly Func<Vector2> _initialScale;
+    private readonly IEasing _easing;
+
+    public ExpandInEnterTransitionImpl(Func<Vector2> initialScale, IEasing? easing)
+    {
+        _initialScale = initialScale;
+        _easing = easing ?? EaseInOut;
+    }
+
+    public IModifier Get(float progress, LayoutInfo parent)
+    {
+        var resolvedProgress = _easing.Transform(progress);
+        return Modifier
+            .Scale(
+                Vector2.Lerp(_initialScale(), Vector2.one, resolvedProgress)
+            );
+    }
+}
---
+++ Assets/Packages/UnityCompose/Runtime/Api/Transitions/Exit/ShrinkOutExitTransitionImpl.cs
@@ -0,0 +1,57 @@
+﻿// ReSharper disable CheckNamespace
+
+using System;
+using UnityEngine;
+
+namespace UnityCompose;
+
+public static partial class ComposeFunctions
+{
+    public static IExitTransition ShrinkOut(
+        Func<Vector2>? targetScale = null,
+        IEasing? easing = null
+    )
+    {
+        return new ShrinkOutExitTransitionImpl(targetScale ?? (() => Vector2.zero), easing);
+    }
+
+    public static IExitTransition ShrinkOutHorizontally(
+        Func<float>? targetScaleX = null,
+        IEasing? easing = null
+    )
+    {
+        var scaleDelegate = targetScaleX ?? (() => 0f);
+        return new ShrinkOutExitTransitionImpl(() => new Vector2(scaleDelegate(), 1f), easing);
+    }
+
+
+    public static IExitTransition ShrinkOutVertically(
+        Func<float>? targetScaleY = null,
+        IEasing? easing = null
+    )
+    {
+        var scaleDelegate = targetScaleY ?? (() => 0f);
+        return new ShrinkOutExitTransitionImpl(() => new Vector2(1f, scaleDelegate()), easing);
+    }
+}
+
+internal class ShrinkOutExitTransitionImpl : IExitTransition
+{
+    private readonly Func<Vector2> _targetScale;
+    private readonly IEasing _easing;
+
+    public ShrinkOutExitTransitionImpl(Func<Vector2> targetScale, IEasing? easing)
+    {
+        _targetScale = targetScale;
+        _easing = easing ?? EaseInOut;
+    }
+
+    public IModifier Get(float progress, LayoutInfo parent)
+    {
+        var resolvedProgress = _easing.Transform(progress);
+        return Modifier
+            .Scale(
+                Vector2.Lerp(Vector2.one, _targetScale(), resolvedProgress)
+            );
+    }
+}
