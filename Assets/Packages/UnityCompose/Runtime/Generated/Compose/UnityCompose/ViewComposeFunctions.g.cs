1ac355bdaf7d4a24654e8b2656c484506cfc80f2
Kolotilov Yaroslav <kolotilov_yp@magnit.ru>
2025-11-05 16:36:55 +0300

    Text edits

--- Assets/Packages/UnityCompose/Runtime/Generated/Compose/UnityCompose/ViewComposeFunctions.g.cs
+++ Assets/Packages/UnityCompose/Runtime/Generated/Compose/UnityCompose/ViewComposeFunctions.g.cs
@@ -152,23 +152,52 @@ public static partial class ComposeFunctions
             return;
         try
         {
-            ReusableComposeView<Text>(modifier: modifier, initializer: Remember<global::System.Action<global::UnityCompose.Packages.UnityCompose.Runtime.Impl.Views.Text>>((text, color, fontSize, style, fontStyle, fontWeight, softWrap, textAlign), it =>
+            var localContentColor = LocalContentColor.Current;
+            var localTextStyle = LocalTextStyle.Current;
+            ReusableComposeView<Text>(modifier: modifier, initializer: Remember<global::System.Action<global::UnityCompose.Packages.UnityCompose.Runtime.Impl.Views.Text>>((text, color, fontSize, style, fontStyle, fontWeight, softWrap, textAlign, localContentColor, localTextStyle), it =>
             {
                 it.text = text;
                 it.style.whiteSpace = softWrap ? WhiteSpace.Normal : WhiteSpace.NoWrap;
-                var resolvedFontStyle = fontStyle.HasValue ? fontStyle.Value : style.HasValue ? style.Value.FontStyle : FontStyle.Normal;
-                var resolvedFontWeight = fontWeight.HasValue ? fontWeight.Value : style.HasValue ? style.Value.FontWeight : FontWeight.Normal;
+                // FontStyle
+                FontStyle resolvedFontStyle;
+                if (fontStyle.HasValue)
+                    resolvedFontStyle = fontStyle.Value;
+                else if (style.HasValue)
+                    resolvedFontStyle = style.Value.FontStyle;
+                else if (localTextStyle.HasValue)
+                    resolvedFontStyle = localTextStyle.Value.FontStyle;
+                else
+                    resolvedFontStyle = FontStyle.Normal;
+                // FontWeight
+                FontWeight resolvedFontWeight;
+                if (fontWeight.HasValue)
+                    resolvedFontWeight = fontWeight.Value;
+                else if (style.HasValue)
+                    resolvedFontWeight = style.Value.FontWeight;
+                else if (localTextStyle.HasValue)
+                    resolvedFontWeight = localTextStyle.Value.FontWeight;
+                else
+                    resolvedFontWeight = FontWeight.Normal;
                 it.style.unityFontStyleAndWeight = FontStyleUtils.ToUnityFontStyle(resolvedFontStyle, resolvedFontWeight);
                 it.style.unityTextAlign = textAlign.ToTextAnchor();
-                it.style.fontSize = fontSize.GetOrDefault(style.HasValue ? style.Value.FontSize : 14f);
+                // FontSize
+                if (fontSize.HasValue)
+                    it.style.fontSize = fontSize.Value;
+                else if (style.HasValue)
+                    it.style.fontSize = style.Value.FontSize;
+                else if (localTextStyle.HasValue)
+                    it.style.fontSize = localTextStyle.Value.FontSize;
+                else
+                    it.style.fontSize = 14f;
+                // Color
                 if (color.HasValue)
                     it.style.color = color.Value;
                 else if (style is { HasValue: true, Value.Color.HasValue: true })
                     it.style.color = style.Value.Color.Value;
-                else if (LocalContentColor.Current.HasValue)
-                    it.style.color = LocalContentColor.Current.Value;
-                else if (LocalTextStyle.Current is { HasValue: true, Value.Color.HasValue: true })
-                    it.style.color = LocalTextStyle.Current.Value.Color.Value;
+                else if (localContentColor.HasValue)
+                    it.style.color = localContentColor.Value;
+                else if (localTextStyle is { HasValue: true, Value.Color.HasValue: true })
+                    it.style.color = localTextStyle.Value.Color.Value;
                 else
                     it.style.color = Color.black;
             }));
