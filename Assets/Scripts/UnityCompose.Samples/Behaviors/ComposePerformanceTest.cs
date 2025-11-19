8e4c5df1333a674270ac7c9454c865b31427069c
Kolotilov Yaroslav <kolotilov_yp@magnit.ru>
2025-11-19 22:47:41 +0300

    Performance Test WIP

---
+++ Assets/Scripts/UnityCompose.Samples/Behaviors/ComposePerformanceTest.cs
@@ -0,0 +1,68 @@
+﻿// ReSharper disable ArrangeNamespaceBody
+
+using StableCollections;
+using UnityEngine.UIElements;
+
+namespace UnityCompose.Samples.Behaviors
+{
+    internal partial class ComposePerformanceTest : ComposeUI
+    {
+        [Composable]
+        protected override void Content()
+        {
+            var parentSize = Remember(() => IMutableStableProperty.Create(Vector2.zero));
+            Box(
+                modifier: Modifier
+                    .FillMaxSize()
+                    .OnGloballyPositioned(it => parentSize.Value = it.SizeWithPaddings),
+                content: () =>
+                {
+                    for (var i = 0; i < 1000; i++)
+                    {
+                        var currentI = i;
+                        Key(
+                            key: currentI,
+                            content: () =>
+                            {
+                                var position = Remember(static () => MutableStateOf(Vector2.zero));
+                                LaunchedEffect(
+                                    key: string.Empty,
+                                    coroutine: () => PerformanceUtils.MoveRandomlyCoroutine(
+                                        parentSize: () => parentSize.Value,
+                                        it => position.Value = it
+                                    )
+                                );
+
+                                Spacer(
+                                    modifier: Modifier
+                                        .Size(50)
+                                        .Background(PerformanceUtils.Colors[currentI % PerformanceUtils.Colors.Length])
+                                        .Float()
+                                        .Position(
+                                            left: position.Value.x,
+                                            top: position.Value.y
+                                        )
+                                );
+                            }
+                        );
+                    }
+
+                    var fps = Remember(() => MutableStateOf(0));
+                    LaunchedEffect(string.Empty, () => PerformanceUtils.MeasureFpsCoroutine(it => fps.Value = it));
+
+                    Text(
+                        text: fps.Value.ToString(),
+                        color: Color.white,
+                        modifier: Modifier
+                            .Float()
+                            .Background(Color.black)
+                            .Position(
+                                right: 40,
+                                top: 40
+                            )
+                    );
+                }
+            );
+        }
+    }
+}
---
+++ Assets/Scripts/UnityCompose.Samples/Behaviors/PerformanceUtils.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using Random = System.Random;
+
+namespace UnityCompose.Samples.Behaviors;
+
+public static class PerformanceUtils
+{
+    public static readonly Color[] Colors =
+    {
+        Color.red,
+        Color.green,
+        Color.blue,
+        Color.yellow,
+        Color.cyan,
+        Color.magenta,
+        Color.white,
+        Color.gray,
+    };
+    
+    public static IEnumerator MoveRandomlyCoroutine(Func<Vector2> parentSize, Action<Vector2> onValueChanged)
+    {
+        while (float.IsNaN(parentSize().x) || float.IsNaN(parentSize().y))
+            yield return null;
+
+        var current = new Vector2(
+            UnityEngine.Random.Range(0f, parentSize().x),
+            UnityEngine.Random.Range(0f, parentSize().y)
+        );
+
+        while (true)
+        {
+            var target = new Vector2(
+                UnityEngine.Random.Range(0f, parentSize().x),
+                UnityEngine.Random.Range(0f, parentSize().y)
+            );
+
+            var elapsed = 0f;
+
+            var interval = UnityEngine.Random.Range(0.2f, 0.8f);
+            while (elapsed < interval)
+            {
+                elapsed += Time.deltaTime;
+                var t = Mathf.Clamp01(elapsed / interval);
+                var value = Vector2.Lerp(current, target, t);
+                onValueChanged?.Invoke(value);
+
+                yield return null;
+            }
+
+            onValueChanged?.Invoke(target);
+            current = target;
+        }
+    }
+    
+    public static IEnumerator MeasureFpsCoroutine(Action<int> onValueChanged)
+    {
+        var interval = 1f;
+
+        var frames = 0;
+        var elapsed = 0f;
+
+        while (true)
+        {
+            frames++;
+            elapsed += Time.deltaTime;
+
+            if (elapsed >= interval)
+            {
+                var fps = Mathf.RoundToInt(frames / elapsed);
+                onValueChanged?.Invoke(fps);
+
+                frames = 0;
+                elapsed = 0f;
+            }
+
+            yield return null;
+        }
+    }
+}
