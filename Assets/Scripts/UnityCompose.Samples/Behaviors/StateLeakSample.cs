bac3d52848079277929575c79769d1a623ef2859
Kolotilov Yaroslav <kolotilov_yp@magnit.ru>
2025-12-18 15:25:59 +0300

    StateLeakSample

--- Assets/Scripts/UnityCompose.Samples/Behaviors/StateLeakSample.cs
+++ Assets/Scripts/UnityCompose.Samples/Behaviors/StateLeakSample.cs
@@ -17,59 +17,70 @@ namespace UnityCompose.Samples.Behaviors
         [Composable]
         private static void Layout()
         {
-            Box(
-                horizontalAlignment: Alignment.Horizontal.Center,
-                verticalAlignment: Alignment.Vertical.Center,
-                modifier: Modifier
-                    .FillMaxSize(),
+            CompositionLocalProvider(
+                LocalTextStyle.Provides(
+                    new TextStyle(
+                        FontSize: 20,
+                        Color: Color.white
+                    )
+                ),
                 content: () =>
                 {
-                    Column(
-                        modifier: Modifier,
+                    Box(
+                        horizontalAlignment: Alignment.Horizontal.Center,
+                        verticalAlignment: Alignment.Vertical.Center,
+                        modifier: Modifier
+                            .FillMaxSize(),
                         content: () =>
                         {
-                            var showFirst = Remember(() => MutableStateOf(false));
-                            if (showFirst.Value)
-                            {
-                                var firstCount = Remember(() => MutableStateOf(0));
-                                Text(
-                                    text: $"Clicked {firstCount.Value} times",
-                                    textAlign: TextAlign.MiddleCenter,
-                                    modifier: Modifier
-                                        .FillMaxWidth()
-                                        .Background(Color.red)
-                                        .Padding(all: 20)
-                                        .Border(radius: 16)
-                                        .OnClick(() => firstCount.Value++)
-                                        .Name("first-button")
-                                );
-                            }
+                            Column(
+                                modifier: Modifier,
+                                content: () =>
+                                {
+                                    var showFirst = Remember(() => MutableStateOf(false));
+                                    if (showFirst.Value)
+                                    {
+                                        var firstCount = Remember(() => MutableStateOf(0));
+                                        Text(
+                                            text: $"Clicked {firstCount.Value} times",
+                                            textAlign: TextAlign.MiddleCenter,
+                                            modifier: Modifier
+                                                .FillMaxWidth()
+                                                .Background(Color.red)
+                                                .Padding(all: 20)
+                                                .Border(radius: 16)
+                                                .OnClick(() => firstCount.Value++)
+                                                .Name("first-button")
+                                        );
+                                    }
 
-                            var secondCount = Remember(() => MutableStateOf(0));
-                            Text(
-                                text: $"Clicked {secondCount.Value} times",
-                                textAlign: TextAlign.MiddleCenter,
-                                modifier: Modifier
-                                    .FillMaxWidth()
-                                    .Background(Color.green)
-                                    .Padding(all: 20)
-                                    .Border(radius: 16)
-                                    .Margin(top: 16)
-                                    .OnClick(() => secondCount.Value++)
-                                    .Name("second-button")
-                            );
+                                    var secondCount = Remember(() => MutableStateOf(0));
+                                    Text(
+                                        text: $"Clicked {secondCount.Value} times",
+                                        textAlign: TextAlign.MiddleCenter,
+                                        modifier: Modifier
+                                            .FillMaxWidth()
+                                            .Background(Color.green)
+                                            .Padding(all: 20)
+                                            .Border(radius: 16)
+                                            .Margin(top: 16)
+                                            .OnClick(() => secondCount.Value++)
+                                            .Name("second-button")
+                                    );
 
-                            Text(
-                                text: "Switch",
-                                textAlign: TextAlign.MiddleCenter,
-                                modifier: Modifier
-                                    .FillMaxWidth()
-                                    .Background(Color.blue)
-                                    .Padding(all: 20)
-                                    .Border(radius: 16)
-                                    .Margin(top: 16)
-                                    .OnClick(() => showFirst.Value = !showFirst.Value)
-                                    .Name("switch-button")
+                                    Text(
+                                        text: "Switch",
+                                        textAlign: TextAlign.MiddleCenter,
+                                        modifier: Modifier
+                                            .FillMaxWidth()
+                                            .Background(Color.blue)
+                                            .Padding(all: 20)
+                                            .Border(radius: 16)
+                                            .Margin(top: 16)
+                                            .OnClick(() => showFirst.Value = !showFirst.Value)
+                                            .Name("switch-button")
+                                    );
+                                }
                             );
                         }
                     );
--- Assets/Scripts/UnityCompose.Samples/Generated/Compose/UnityCompose.Samples.Behaviors/StateLeakSample.g.cs
+++ Assets/Scripts/UnityCompose.Samples/Generated/Compose/UnityCompose.Samples.Behaviors/StateLeakSample.g.cs
@@ -14,22 +14,25 @@ namespace UnityCompose.Samples.Behaviors
             __composer.StartRestartGroup(2053751605);
             if (__composer.ShouldExecute())
             {
-                Box(horizontalAlignment: Alignment.Horizontal.Center, verticalAlignment: Alignment.Vertical.Center, modifier: Modifier.FillMaxSize(), content: !__composer.Changed() ? __composer.RememberedValue<UnityCompose.ComposableContent>() : __composer.UpdateRememberedValue<UnityCompose.ComposableContent>(() =>
+                CompositionLocalProvider(LocalTextStyle.Provides(new TextStyle(FontSize: 20, Color: Color.white)), content: !__composer.Changed() ? __composer.RememberedValue<UnityCompose.ComposableContent>() : __composer.UpdateRememberedValue<UnityCompose.ComposableContent>(() =>
                 {
-                    Column(modifier: Modifier, content: !__composer.Changed() ? __composer.RememberedValue<UnityCompose.ComposableContent>() : __composer.UpdateRememberedValue<UnityCompose.ComposableContent>(() =>
+                    Box(horizontalAlignment: Alignment.Horizontal.Center, verticalAlignment: Alignment.Vertical.Center, modifier: Modifier.FillMaxSize(), content: !__composer.Changed() ? __composer.RememberedValue<UnityCompose.ComposableContent>() : __composer.UpdateRememberedValue<UnityCompose.ComposableContent>(() =>
                     {
-                        var showFirst = !__composer.Changed() ? __composer.RememberedValue<UnityCompose.IMutableState<bool>>() : __composer.UpdateRememberedValue<UnityCompose.IMutableState<bool>>(MutableStateOf(false));
-                        __composer.StartReplaceGroup(1013871575);
-                        if (showFirst.Value)
+                        Column(modifier: Modifier, content: !__composer.Changed() ? __composer.RememberedValue<UnityCompose.ComposableContent>() : __composer.UpdateRememberedValue<UnityCompose.ComposableContent>(() =>
                         {
-                            var firstCount = !__composer.Changed() ? __composer.RememberedValue<UnityCompose.IMutableState<int>>() : __composer.UpdateRememberedValue<UnityCompose.IMutableState<int>>(MutableStateOf(0));
-                            Text(text: $"Clicked {firstCount.Value} times", textAlign: TextAlign.MiddleCenter, modifier: Modifier.FillMaxWidth().Background(Color.red).Padding(all: 20).Border(radius: 16).OnClick(!__composer.Changed(firstCount) ? __composer.RememberedValue<System.Action>() : __composer.UpdateRememberedValue<System.Action>(() => firstCount.Value++)).Name("first-button"));
-                        }
+                            var showFirst = !__composer.Changed() ? __composer.RememberedValue<UnityCompose.IMutableState<bool>>() : __composer.UpdateRememberedValue<UnityCompose.IMutableState<bool>>(MutableStateOf(false));
+                            __composer.StartReplaceGroup(2135206348);
+                            if (showFirst.Value)
+                            {
+                                var firstCount = !__composer.Changed() ? __composer.RememberedValue<UnityCompose.IMutableState<int>>() : __composer.UpdateRememberedValue<UnityCompose.IMutableState<int>>(MutableStateOf(0));
+                                Text(text: $"Clicked {firstCount.Value} times", textAlign: TextAlign.MiddleCenter, modifier: Modifier.FillMaxWidth().Background(Color.red).Padding(all: 20).Border(radius: 16).OnClick(!__composer.Changed(firstCount) ? __composer.RememberedValue<System.Action>() : __composer.UpdateRememberedValue<System.Action>(() => firstCount.Value++)).Name("first-button"));
+                            }
 
-                        __composer.EndReplaceGroup(1013871575);
-                        var secondCount = !__composer.Changed() ? __composer.RememberedValue<UnityCompose.IMutableState<int>>() : __composer.UpdateRememberedValue<UnityCompose.IMutableState<int>>(MutableStateOf(0));
-                        Text(text: $"Clicked {secondCount.Value} times", textAlign: TextAlign.MiddleCenter, modifier: Modifier.FillMaxWidth().Background(Color.green).Padding(all: 20).Border(radius: 16).Margin(top: 16).OnClick(!__composer.Changed(secondCount) ? __composer.RememberedValue<System.Action>() : __composer.UpdateRememberedValue<System.Action>(() => secondCount.Value++)).Name("second-button"));
-                        Text(text: "Switch", textAlign: TextAlign.MiddleCenter, modifier: Modifier.FillMaxWidth().Background(Color.blue).Padding(all: 20).Border(radius: 16).Margin(top: 16).OnClick(!__composer.Changed(showFirst) ? __composer.RememberedValue<System.Action>() : __composer.UpdateRememberedValue<System.Action>(() => showFirst.Value = !showFirst.Value)).Name("switch-button"));
+                            __composer.EndReplaceGroup(2135206348);
+                            var secondCount = !__composer.Changed() ? __composer.RememberedValue<UnityCompose.IMutableState<int>>() : __composer.UpdateRememberedValue<UnityCompose.IMutableState<int>>(MutableStateOf(0));
+                            Text(text: $"Clicked {secondCount.Value} times", textAlign: TextAlign.MiddleCenter, modifier: Modifier.FillMaxWidth().Background(Color.green).Padding(all: 20).Border(radius: 16).Margin(top: 16).OnClick(!__composer.Changed(secondCount) ? __composer.RememberedValue<System.Action>() : __composer.UpdateRememberedValue<System.Action>(() => secondCount.Value++)).Name("second-button"));
+                            Text(text: "Switch", textAlign: TextAlign.MiddleCenter, modifier: Modifier.FillMaxWidth().Background(Color.blue).Padding(all: 20).Border(radius: 16).Margin(top: 16).OnClick(!__composer.Changed(showFirst) ? __composer.RememberedValue<System.Action>() : __composer.UpdateRememberedValue<System.Action>(() => showFirst.Value = !showFirst.Value)).Name("switch-button"));
+                        }));
                     }));
                 }));
             }
