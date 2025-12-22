15dfacd304cd7f4f4debb265a42483258a66881a
Kolotilov Yaroslav <kolotilov_yp@magnit.ru>
2025-12-22 15:17:24 +0300

    Key WIP

--- Assets/Packages/UnityCompose/Runtime/Api/Functions/KeyComposeFunctions.cs
+++ Assets/Packages/UnityCompose/Runtime/Api/Functions/KeyComposeFunctions.cs
@@ -4,13 +4,24 @@ namespace UnityCompose;
 
 public static partial class ComposeFunctions
 {
-    [Composable]
+    [Composable, Compiled]
     public static void Key<T>(
         T key,
         ComposableContent content
     )
     {
-        // BRUH
-        content();
+        var composer = CurrentComposer;
+        composer.StartRestartGroup(4357447, key);
+        var isRestarted = composer.IsRestarted();
+        if (isRestarted || composer.ShouldExecuteAsStruct((key, content)))
+        {
+            content();
+        }
+        else
+        {
+            composer.SkipToGroupEnd();
+        }
+
+        composer.EndRestartGroup(4357447, isRestarted)?.UpdateScope(() => Key(key, content));
     }
 }
