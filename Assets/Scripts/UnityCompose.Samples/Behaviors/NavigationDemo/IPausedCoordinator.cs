9ae2b3cf5f9b83aeb694446cca2f9c7a8c612921
Nikita Kaminov <nikitakaminov@gmail.com>
2026-08-19 00:01:41 +0300

    Revert "Try: GapBufferList Fix"
    This reverts commit 29fd2db01de3166a35e1bf6863bec27d5045fb2b.

--- Assets/Scripts/UnityCompose.Samples/Behaviors/NavigationDemo/IPausedCoordinator.cs
+++ Assets/Scripts/UnityCompose.Samples/Behaviors/NavigationDemo/IPausedCoordinator.cs
@@ -23,7 +23,6 @@ internal class PausedCoordinatorImpl : BaseComposeCoordinator, IPausedCoordinato
         ComposeScreen screen = tab switch
         {
             PausedTab.Inventory => new InventoryTabScreen(),
-            PausedTab.System => new SystemFlowScreen(),
             _ => new PausedTabScreen(tab, ResolveBackgroundColor(tab))
         };
         Router.ReplaceScreen(screen);
