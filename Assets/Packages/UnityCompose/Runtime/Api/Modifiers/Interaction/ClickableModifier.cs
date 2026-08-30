5657f6fd6a448066a89652291ec7e58920c246ec
Kolotilov Yaroslav <kolotilov_yp@magnit.ru>
2026-08-30 19:28:38 +0300

    Clickable & Hoverable

---
+++ Assets/Packages/UnityCompose/Runtime/Api/Modifiers/Interaction/ClickableModifier.cs
@@ -0,0 +1,87 @@
+﻿// ReSharper disable CheckNamespace
+
+using SharpExtensions;
+using StableCollections;
+using UnityCompose.Packages.UnityCompose.Runtime.Impl.Utils;
+using UnityEngine.UIElements;
+
+namespace UnityCompose;
+
+public static partial class ModifierExtensions
+{
+    public static IModifier Clickable(
+        this IModifier modifier,
+        IMutableInteractionSource interactionSource,
+        bool enabled = true
+    )
+    {
+        if (!enabled)
+            return modifier;
+        return modifier + new ClickableModifierImpl(interactionSource);
+    }
+}
+
+public static partial class VisualElementExtensions
+{
+    private const string PressInteractionsKey = "UnityCompose_PressInteractions";
+
+    internal static IMutableStableList<IPressInteraction.Press> PressInteractions(this VisualElement element)
+    {
+        return (IMutableStableList<IPressInteraction.Press>)element.UserData().GetOrPut(
+            PressInteractionsKey,
+            static () => MutableStableListOf<IPressInteraction.Press>()
+        ).NotNull();
+    }
+}
+
+internal class ClickableModifierImpl : BaseModifier<ClickableModifierImpl>
+{
+    private readonly IMutableInteractionSource _interactionSource;
+    private readonly EventCallback<PointerDownEvent> _pointerDownCallback;
+    private readonly EventCallback<PointerUpEvent> _pointerUpCallback;
+
+    public ClickableModifierImpl(IMutableInteractionSource interactionSource)
+    {
+        _interactionSource = interactionSource;
+        _pointerDownCallback = OnPointerDown;
+        _pointerUpCallback = OnPointerUp;
+    }
+
+    public override void Apply(VisualElement element)
+    {
+        element.RegisterCallback(_pointerDownCallback);
+        element.RegisterCallback(_pointerUpCallback);
+    }
+
+    public override void Revert(VisualElement element)
+    {
+        element.UnregisterCallback(_pointerDownCallback);
+        element.UnregisterCallback(_pointerUpCallback);
+    }
+
+    protected override bool Equals(ClickableModifierImpl other)
+    {
+        return _interactionSource == other._interactionSource;
+    }
+
+    private void OnPointerDown(PointerDownEvent evt)
+    {
+        if (evt.button != 0)
+            return;
+        var pressInteraction = new IPressInteraction.Press(evt.localPosition);
+        evt.VisualElement().PressInteractions().Add(pressInteraction);
+        _interactionSource.Emit(pressInteraction);
+    }
+
+    private void OnPointerUp(PointerUpEvent evt)
+    {
+        if (evt.button != 0)
+            return;
+        var pressInteractions = evt.VisualElement().PressInteractions();
+        if (pressInteractions.IsEmpty())
+            return;
+        var pressInteraction = pressInteractions[0];
+        pressInteractions.RemoveAt(0);
+        _interactionSource.Emit(new IPressInteraction.Release(pressInteraction));
+    }
+}
---
+++ Assets/Packages/UnityCompose/Runtime/Api/Modifiers/Interaction/HoverableModifier.cs
@@ -0,0 +1,85 @@
+﻿// ReSharper disable CheckNamespace
+
+using SharpExtensions;
+using StableCollections;
+using UnityCompose.Packages.UnityCompose.Runtime.Impl.Utils;
+using UnityEngine.UIElements;
+
+namespace UnityCompose;
+
+public static partial class ModifierExtensions
+{
+    public static IModifier Hoverable(
+        this IModifier modifier,
+        IMutableInteractionSource interactionSource,
+        bool enabled = true
+    )
+    {
+        if (!enabled)
+            return modifier;
+        return modifier + new HoverableModiferImpl(interactionSource);
+    }
+}
+
+public static partial class VisualElementExtensions
+{
+    private const string EnterInteractionsKey = "UnityCompose_HoverEvents";
+
+    internal static IMutableStableList<IHoverInteraction.Enter> EnterInteractions(this VisualElement element)
+    {
+        return (IMutableStableList<IHoverInteraction.Enter>)element.UserData().GetOrPut(
+            EnterInteractionsKey,
+            static () => MutableStableListOf<IHoverInteraction.Enter>()
+        ).NotNull();
+    }
+}
+
+internal class HoverableModiferImpl : BaseModifier<HoverableModiferImpl>
+{
+    private readonly IMutableInteractionSource _interactionSource;
+    private readonly EventCallback<PointerEnterEvent> _pointerEnterCallback;
+    private readonly EventCallback<PointerLeaveEvent> _pointerLeaveCallback;
+    
+    public HoverableModiferImpl(IMutableInteractionSource interactionSource)
+    {
+        _interactionSource = interactionSource;
+        _pointerEnterCallback = OnPointerEnter;
+        _pointerLeaveCallback = OnPointerLeave;
+    }
+
+    public override void Apply(VisualElement element)
+    {
+        element.RegisterCallback(_pointerEnterCallback);
+        element.RegisterCallback(_pointerLeaveCallback);
+    }
+
+    public override void Revert(VisualElement element)
+    {
+        element.UnregisterCallback(_pointerEnterCallback);
+        element.UnregisterCallback(_pointerLeaveCallback);
+    }
+
+    protected override bool Equals(HoverableModiferImpl other)
+    {
+        return _interactionSource == other._interactionSource;
+    }
+
+    private void OnPointerEnter(PointerEnterEvent evt)
+    {
+        var visualElement = evt.VisualElement();
+        var enterInteraction = new IHoverInteraction.Enter();
+        visualElement.EnterInteractions().Add(enterInteraction);
+        _interactionSource.Emit(enterInteraction);
+    }
+
+    private void OnPointerLeave(PointerLeaveEvent evt)
+    {
+        var visualElement = evt.VisualElement();
+        var enterInteractions = visualElement.EnterInteractions();
+        if (enterInteractions.IsEmpty())
+            return;
+        var enterInteraction = enterInteractions[enterInteractions.LastIndex];
+        enterInteractions.RemoveAt(enterInteractions.LastIndex);
+        _interactionSource.Emit(new IHoverInteraction.Exit(enterInteraction));
+    }
+}
