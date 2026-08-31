4aba4d35bac073d9cb8bc1bd54f9c858d93fdabb
Nikita Kaminov <nikitakaminov@gmail.com>
2026-08-31 19:52:44 +0300

    Constructors

--- Assets/Packages/UnityCompose/Runtime/Api/Models/PaddingValues.cs
+++ Assets/Packages/UnityCompose/Runtime/Api/Models/PaddingValues.cs
@@ -1,8 +1,23 @@
-﻿namespace UnityCompose.Packages.UnityCompose.Runtime.Api.Models;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace UnityCompose.Packages.UnityCompose.Runtime.Api.Models;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
 public readonly record struct PaddingValues(
-    Dp Top,
-    Dp Bottom,
-    Dp Left,
-    Dp Right
-);
+    Dp Top = default,
+    Dp Bottom = default,
+    Dp Left = default,
+    Dp Right = default
+)
+{
+    public PaddingValues(Dp All) : this(All, All, All, All)
+    {
+    }
+
+    public PaddingValues(
+        Dp Horizontal = default,
+        Dp Vertical = default
+    ) : this(Vertical, Vertical, Horizontal, Horizontal)
+    {
+    }
+}
--- Assets/Packages/UnityCompose/Runtime/Api/Models/RoundedCornerShape.cs
+++ Assets/Packages/UnityCompose/Runtime/Api/Models/RoundedCornerShape.cs
@@ -1,10 +1,20 @@
 ﻿// ReSharper disable CheckNamespace
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace UnityCompose;
 
+[SuppressMessage("ReSharper", "InconsistentNaming")]
 public readonly record struct RoundedCornerShape(
     LayoutLength TopLeft = default,
     LayoutLength TopRight = default,
     LayoutLength BottomLeft = default,
     LayoutLength BottomRight = default
-);
+)
+{
+    public RoundedCornerShape(
+        LayoutLength All
+    ) : this(All, All, All, All)
+    {
+    }
+}
