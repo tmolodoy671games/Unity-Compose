4e5c5435dbf6951a4770841c4a4263b99757be49
Nikita Kaminov <nikitakaminov@gmail.com>
2026-09-22 01:09:18 +0300

    WIP UNSTABLE

---
+++ Assets/Packages/UnityCompose/Foo.cs
@@ -0,0 +1,12 @@
+﻿using UnityCompose;
+
+namespace System.Runtime.CompilerServices.CodeGen
+{
+    internal partial class Foo
+    {
+        [Composable]
+        void Bar()
+        {
+        }
+    }
+}
